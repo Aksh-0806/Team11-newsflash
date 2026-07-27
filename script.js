a5f94ccfbe8279a78df5e194cb9136c95faef2d6
sivapriyasl07 <sivapriyasl200507@gmail.com>
2026-07-27 20:01:39 +0530

    add news feed and category filter

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 
+const newsContainer = document.getElementById("news-container");
+
+function loadNews(tag) {
+    newsContainer.innerHTML = "<p>Loading...</p>";
+
+    fetch(`https://dev.to/api/articles?tag=${tag}`)
+        .then(response => response.json())
+        .then(data => {
+            newsContainer.innerHTML = "";
+
+            data.forEach(article => {
+                newsContainer.innerHTML += `
+                    <div class="card">
+                        <h3>${article.title}</h3>
+                        <p>${article.description || "No description available."}</p>
+                        <a href="${article.url}" target="_blank">Read More</a>
+                    </div>
+                `;
+            });
+        })
+        .catch(() => {
+            newsContainer.innerHTML = "<p>Couldn't fetch data.</p>";
+        });
+}
+
+// Load JavaScript articles when the page opens
+loadNews("javascript");
